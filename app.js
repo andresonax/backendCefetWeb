c2d8dbc5cfda22d03a1470988897f9a3b654e718
andresonax <andresonax@gmail.com>
2023-08-31 08:10:44 -0300

    aula 2508

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000
 const bodyParser = require("body-parser")
 const moment = require("moment")
 
+
 app.use(bodyParser.urlencoded({extended : true}))
 
 const connection = mysql.createConnection({
@@ -28,27 +29,67 @@ app.get('/clientes', (req, res) => {
   );
 })
 
-app.post('/clientes', (req, res) => {
-  var nome = req.body.nome
-  var sobrenome = req.body.sobrenome
-  var sql = 'INSERT INTO cliente' +
-            '  (nome, sobrenome, data_cadastro)' +
-            '  VALUES ("'+ nome + '","' + sobrenome +'", "'+ moment().format("YYYY-MM-DD") +'");'
-  
-  var sqlStringInt = `insert into cliente(nome, sobrenome, data_cadastro)` +
-      `values("${nome}", "${sobrenome}", "${moment().format("YYYY-MM-DD")}")`            
-  console.log("consulta sql", sql)
+app.get('/clientes/:id_cliente', (req, res) => {
+  var id_cliente = req.params.id_cliente 
   connection.query(
-    sqlStringInt, (err, results, fields) => {
+    `select * from cliente where id_cliente = ${id_cliente}`,
+    (err, results, fields) => {
       if(err) console.log(err)
       res.send(results)
     }
   );
-
-
-  // res.send("funcionando")
-
 })
+app.get('/clientes_email/:email', (req, res) => {
+  var email = req.params.email 
+  var sql =  `select * from cliente where email = "${email}"`
+  connection.query(
+    sql,
+    (err, results, fields) => {
+      if(err) console.log(err)
+      console.log(results)
+      if(results.length > 0) res.send({existe : true})
+      else res.send({existe : false})
+    }
+  );
+})
+
+app.post('/clientes_del/:id_cliente', (req, res) => {
+  var id_cliente = req.params.id_cliente 
+  connection.query(
+    `delete from cliente where id_cliente = ${id_cliente}`,
+    (err, results, fields) => {
+      if(err) console.log(err)
+      res.send(results)
+    }
+  );
+})
+
+app.post('/clientes',  (req, res) => {
+  var nome = req.body.nome
+  var sobrenome = req.body.sobrenome
+  var email = req.body.email
+  var data_cadastro = moment().format("YYYY-MM-DD")
+  var salario = req.body.salario
+  console.log(req.files)
+  var sql = `insert into cliente(nome, sobrenome, email, `+
+        `data_cadastro,salario) values("${nome}", "${sobrenome}", `+
+        `"${email}", "${data_cadastro}", ${salario})`
+   connection.query(sql, (erro, resultado) =>{
+      if(erro) res.send(erro)
+      res.send(resultado)
+   })
+})
+
+app.patch('/clientes/:id_cliente', (req, res) => {
+  var sql = `update cliente set nome = "JoaoAula", `+
+    `sobrenome = "SilvaAula", email = "outro@outro.com", `+
+    `salario = 2500 where id_cliente = 56`
+   connection.query(sql, (erro, resultado) =>{
+      if(erro) res.send(erro)
+      res.send(resultado)
+   })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
